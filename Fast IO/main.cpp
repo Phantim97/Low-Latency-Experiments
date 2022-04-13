1fb81277eab086b9947e1ca7a9094dcb3893787d
Tim Coelho <timothycoelho@gmail.com>
2022-04-13 18:08:45 -0400

    Polished implementation.

--- Fast IO/main.cpp
+++ Fast IO/main.cpp
@@ -1,246 +1,17 @@
 #include <fstream>
-#include <memory>
 #include <vector>
 #include <random>
 #include <omp.h>
 
-#include <benchmark/benchmark.h>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <limits>
+#include <string>
 
 #include "ScopedTimer.h"
 #include "numeric_types.h"
-
-//Prompt:
-/**
- * \brief
- *  Write a program that does the following:
- *	Read a double precision number (from stdin) and print the smallest number that is larger than 95% of numbers encountered so far.
- *	Run program on input size = 128,000,000 and provide the time taken. Provide the time taken for this random input along with the solution.
- * \return 
- */
-
-//Note what they maybe mean by stdin is to calculate it per input as we grow the list
-
-//TODO: Implement indexed version of skip list
-
-template <typename T, typename = numeric_t<T>>
-struct Node
-{
-	T key;
-	Node<T>** forward; //Array to hold pointers
-
-	Node(T key, const int level)
-	{
-		this->key = key;
-		forward = new Node<T>*[level + 1];
-
-		memset(forward, 0, sizeof(Node<T>*) * (level + 1));
-	}
-
-	~Node()
-	{
-		delete[] forward;
-	}
-};
-
-static enum class Scale
-{
-	larger_than,
-	smaller_than,
-};
-
-template<typename T, typename = numeric_t<T>>
-class SkipList
-{
-private:
-	int max_lvl_;
-	float p_; //fraction of nodes with level i pointers having level i+1 pointers
-	Node<T>* header_ = nullptr;
-	int level_;
-	int sz_ = 0;
-
-	int random_level() const
-	{
-		//rand engine
-		std::random_device rd;
-		const std::uniform_real_distribution<T> unif(0, RAND_MAX);
-		std::default_random_engine re(rd());
-		double r = unif(re) / RAND_MAX;
-		int lvl = 0;
-
-		while (r < p_ && lvl < max_lvl_)
-		{
-			lvl++;
-			r = unif(re) / RAND_MAX;
-		}
-
-		return lvl;
-	}
-
-public:
-	SkipList(int max_lvl, float p)
-	{
-		this->max_lvl_ = max_lvl;
-		this->p_ = p;
-		this->level_ = 0;
-		this->header_ = new Node<T>(-1, max_lvl);
-	}
-
-	~SkipList()
-	{
-		delete header_;
-	}
-
-	Node<T>* create_node(T key, int level)
-	{
-		Node<T>* n = new Node<T>(key, level);
-		return n;
-	}
-
-	void insert(T key)
-	{
-		Node<T>* current = header_;
-
-		//Create update array and initialize it
-		Node<T>* update[max_lvl_ + 1];
-		memset(update, 0, sizeof(Node<T>*) * (max_lvl_ + 1));
-
-		for (int i = level_; i >= 0; i--)
-		{
-			while (current->forward[i] != nullptr && current->forward[i]->key < key)
-			{
-				current = current->forward[i];
-			}
-
-			update[i] = current;
-		}
-
-		//Reached level 0 and forward pointer to right which is desired position to insert key
-		current = current->forward[0];
-
-		if (current == nullptr || current->key != key)
-		{
-			const int rlevel = random_level();
-
-			if (rlevel > level_)
-			{
-				for (int i = level_ + 1; i < rlevel + 1; i++)
-				{
-					update[i] = header_;
-				}
-
-				level_ = rlevel;
-			}
-
-			Node<T>* n = create_node(key, rlevel);
-			sz_++;
-
-			//Insert node by rearranging pointers
-			for (int i = 0; i <= rlevel; i++)
-			{
-				n->forward[i] = update[i]->forward[i];
-				update[i]->forward[i] = n;
-			}
-		}
-	}
-
-	void remove(T key)
-	{
-		Node<T>* current = header_;
-
-		Node<T>* update[max_lvl_ + 1];
-		memset(update, 0, sizeof(Node<T>*) * (max_lvl_ + 1));
-
-		for (int i = level_; i >= 0; i--)
-		{
-			while(current->forward[i] != nullptr && current->forward[i]->key < key)
-			{
-				current = current->forward[i];
-			}
-
-			update[i] = current;
-		}
-
-		current = current->forward[0];
-
-		if (current != nullptr && current->key == key)
-		{
-			for (int i = 0; i <= level_; i++)
-			{
-				if (update[i]->forward[i] != current)
-				{
-					break;
-				}
-
-				update[i]->forward[i] = current->forward[i];
-			}
-
-			//Remove levels having no elements
-			while (level_ > 0 && header_->forward[level_] == 0)
-			{
-				level_--;
-			}
-		}
-	}
-
-	void display_list()
-	{
-		for (int i = 0; i <= level_; i++)
-		{
-			Node<T>* node = header_->forward[i];
-			std::cout << "Level: " << i << '\n';
-
-			while (node != nullptr)
-			{
-				std::cout << node->key << " ";
-				node = node->forward[i];
-			}
-
-			std::cout << '\n';
-		}
-	}
-
-	T percentile_find(Scale s, double percent)
-	{
-		percent = sz_ * percent;
-
-		if (percent < 0 || percent > sz)
-		{
-			return DBL_MIN;
-		}
-
-		Node<T>* current = header_;
-		int pos = 0;
-		int tmp = 0;
-
-
-		return curr->value;
-	}
-
-	void find(T key)
-	{
-		Node<T>* current = header_;
-
-		for (int i = level_; i >= 0; i--)
-		{
-			while (current->forward[i] && current->forward[i]->key < key)
-			{
-				current = current->forward[i];
-			}
-		}
-
-		current = current->forward[0];
-
-		if (current && current->key == key)
-		{
-			std::cout << "Found: " << key << '\n';
-		}
-	}
-
-	size_t size() const
-	{
-		return sz_;
-	}
-};
 
 template<typename T, typename = numeric_t<T>>
 static std::vector<T> generate_sample_data(const int sz)
@@ -263,19 +34,6 @@ static std::vector<T> generate_sample_data(const int sz)
 	return a;
 }
 
-//TODO:
-/*
--Structure to capture x percentile range
--calculate on the fly
--Implement a skip list
-*/
-
-#include <iostream>
-#include <vector>
-#include <cassert>
-#include <queue>
-#include <limits>
-
 //SFINAE out all non numeric types for this class
 template <typename T, typename = numeric_t<T>>
 class MinMaxHeap
@@ -286,7 +44,7 @@ private:
 	std::priority_queue<T> max_heap_; //All elements below percentile go here
 	double percentile_ = 0;
 public:
-	MinMaxHeap(const double percentile) : percentile_(percentile)
+	explicit MinMaxHeap(const double percentile) : percentile_(percentile)
 	{
 	}
 
@@ -296,7 +54,7 @@ public:
 		const double total = min_heap_.size() + max_heap_.size();
 		const double ratio = max_heap_.size() / total;
 
-		//If ratio is less than percentile we move it out of the min heap
+		//If ratio is less than percentile we move value out of the min heap
 		if (ratio < percentile_)
 		{
 			const double downgraded = min_heap_.top();
@@ -332,55 +90,72 @@ public:
 		return std::numeric_limits<T>::quiet_NaN();
 	}
 
-	std::size_t size() const
+	size_t size() const
 	{
 		return min_heap_.size() + max_heap_.size();
 	}
+
+	double min_heap_ratio() const
+	{
+		return 1.0 * min_heap_.size() / this->size();
+	}
 };
 
-int main(int argc, char* argv[])
+template <typename T>
+static void add_and_find(MinMaxHeap<T>& heap, std::vector<T>& data)
 {
-	//const double d = std::stod(argv[1]);
-	constexpr double d = 95;
-	MinMaxHeap<double> p(d / 100);
-	double n;
+	ScopedTimer<std::chrono::milliseconds> st;
 
-	//size_t i = 0;
-
-	//while (std::cin >> n)
-	//{
-	//	i++;
-	//	p.insert(n);
-	//	std::cout << p.get() << '\n';
-	//}
-
-	for (int i = 0; i < 10000; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
-		p.insert(i);
+		heap.insert(data[i]);
 	}
 
-	std::cout << "Total Size: " << '\n';
+	std::cout << heap.get() << '\n';
+}
 
-	std::cout << p.get() << '\n';
-	std::cout << p.size() << '\n';
+template <typename T>
+static void heap_populate_and_find(MinMaxHeap<T>& h)
+{
+	ScopedTimer<std::chrono::milliseconds> st;
+	size_t i = 0;
+	double n;
+
+	while (std::cin >> n)
+	{
+		i++;
+		h.insert(n);
+
+		if (i % 2000000 == 0)
+		{
+			std::cout << "Smallest number > 95% of set [" << i <<"]: " << h.get() << '\n';
+		}
+	}
+
+	std::cout << "Smallest number > 95% of set [final]: " << h.get() << '\n';
+}
+
+int main(const int argc, char* argv[])
+{
+	if (argc == 2) //stdin version
+	{
+		const double d = std::stod(argv[1]);
+		MinMaxHeap<double> heap(d / 100);
+		std::cout << "Inserting into list...\n";
+		heap_populate_and_find(heap);
+		std::cout << "Min-Heap ratio to total size: " << heap.min_heap_ratio() << '\n'; //Shows percentage stored in min heap
+	}
+	else //Vector of uniform random values
+	{
+		MinMaxHeap<double> heap(95 / 100.0);
+		std::cout << "Generating dataset...\n";
+		std::vector<double> v = generate_sample_data<double>(128000000);
+		std::cout << "Finding smallest value larger than 95% of set...\n";
+		add_and_find(heap, v);
+		std::cout << "Total Size: " << heap.size() <<'\n';
+		std::cout << "Smallest number > 95% of set: " << heap.get() << '\n';
+		std::cout << "Min-Heap ratio to total size: " << heap.min_heap_ratio() << '\n'; //Shows percentage stored in min heap
+	}
 
 	return 0;
 }
-
-//int main()
-//{
-//	const std::vector<double> data = generate_sample_data<double>(30);
-//	SkipList<double> sl(3, 0.5);
-//
-//	for (int i = 0; i < data.size(); i++)
-//	{
-//		sl.insert(data[i]);
-//	}
-//
-//	sl.display_list();
-//	std::cout << "Size: " << sl.size() << '\n';
-//
-//	std::cout << "Result: " << sl.percentile_find(Scale::larger_than, .95) << '\n';*/
-//
-//	return 0;
-//}
